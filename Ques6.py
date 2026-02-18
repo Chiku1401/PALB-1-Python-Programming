adf386635879d3ebe6609834fde75a9b1c016886
Vansh Sharma <vanshsharma.14.2005@gmail.com>
2026-02-18 23:36:45 +0530

    Delete Ques6.py

--- Ques6.py
+++
@@ -1,9 +0,0 @@
-#Given an array arr, rotate the array by one position in clockwise direction.
-
-class Solution:
-    def rotate(self, arr):
-        if len(arr) == 0:
-            return
-        
-        last = arr.pop()      
-        arr.insert(0, last)
